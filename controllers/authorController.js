21ad3e253b855e7916f4ba666e04c011b2b7f7d4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-09-25 22:01:24 +0530

    Added Create Author Function

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,5 @@
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 const author = require('../models/author')
 const async = require('async')
 const Book = require('../models/book')
@@ -37,12 +39,39 @@ exports.author_detail = (req, res) => {
 
 
 exports.author_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create GET')
+  res.render('author_form', { title: 'Create Author'});
 }
 
-exports.author_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create POST')
-}
+exports.author_create_post = [
+  body('first_name').isLength({min: 1}).trim().withMessage('First name must be specified').isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+  body('family_name').isLength({min: 1}).trim().withMessage('Family name must be specified').isAlphanumeric().withMessage('Family name has non-alphanumeric characters'),
+  body('date_of_birth', 'Invalid date of birth').optional({checkFalsy: true}).isISO8601(),
+  body('date_of_death', 'Invalid date of death').optional({checkFalsy: true}).isISO8601(),
+  
+  sanitizeBody('first_name').escape(),
+  sanitizeBody('family_name').escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      res.render('author_form', {title: 'Create Author', author: req.body, errors: errors})
+      return
+    }else {
+      const authorData = new author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death
+      })
+      authorData.save(function (err) {
+          if (err) { return next(err); }
+          // Successful - redirect to new author record.
+          res.redirect(authorData.url);
+      });
+    }
+  }
+]
 
 exports.author_delete_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author delete GET')
